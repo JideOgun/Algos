14c1c1ec9682b7579ace74a4fb604383480beb5d
JideOgun <JideOgun@github.com>
2022-07-14 17:57:40 -0500

    chapter 2 redux tutorial dataflow...created postslist component and store

--- redux_practice/src/app/store.js
+++ redux_practice/src/app/store.js
@@ -1,7 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "../features/counter/counterSlice";
 
 export const store = configureStore({
-    reducer: {
-        
-    }
-})
+  reducer: { counter: counterReducer },
+});
